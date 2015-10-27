1da4437f5225fc108c25df7ad456980b44f4feb1
Dries Schulten <dries.schulten@gmail.com>
2015-10-27 18:43:35 +0100

    Initial checkin; basic REST API server for c2.

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  spots = require('./routes/spots');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('REST main');
+});
+
+app.get('/spots', spots.findAll);
+app.get('/spots/:id', spots.findById);
+app.post('/spots', spots.addSpot);
+app.put('/spots/:id', spots.updateSpot);
+app.delete('/spots/:id', spots.deleteSpot);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ routes/spots.js
@@ -0,0 +1,103 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+  Db = mongo.Db,
+  ObjectID = mongo.ObjectID;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('c2db', server);
+
+db.open(function (err, db) {
+  if (!err) {
+    console.log('connected to c2db');
+    db.collection('spots', {strict: true}, function (err, collection) {
+      if (err) {
+        console.log('Collection doesn\'t exist, creating it');
+        fillDb();
+      }
+    });
+  }
+});
+
+exports.findAll = function (req, res) {
+  db.collection('spots', function (err, collection) {
+    collection.find().toArray(function (err, items) {
+      res.send(items);
+    });
+  });
+};
+
+exports.findById = function (req, res) {
+  var id = req.params.id;
+  console.log('Loading spot with id: ' + id);
+  db.collection('spots', function (err, collection) {
+    collection.find({'_id': new ObjectID(id)}).limit(1).next(function (err, item) {
+      res.send(item);
+    });
+  });
+};
+
+exports.addSpot = function (req, res) {
+  var spot = req.body;
+  console.log('Adding spot: ' + JSON.stringify(spot));
+  db.collection('spots', function (err, collection) {;
+    collection.insert(spot, {safe: true}, function (err, result) {
+      if (err) {
+        res.send({'error': 'an error has occurred'});
+      } else {
+        console.log('Succes: ' + JSON.stringify(result[0]));
+        res.send(result[0]);
+      }
+    });
+  });
+};
+
+exports.updateSpot = function(req, res) {
+  var id = req.params.id;
+  var spot = req.body;
+  console.log('Updating spot: ' + id);
+  console.log(JSON.stringify(spot));
+  db.collection('spots', function(err, collection) {
+    collection.update({'_id':new ObjectID(id)}, spot, {safe:true}, function(err, result) {
+      if (err) {
+        console.log('Error updating spot: ' + err);
+        res.send({'error':'An error has occurred'});
+      } else {
+        console.log('' + result + ' document(s) updated');
+        res.send(spot);
+      }
+    });
+  });
+};
+
+exports.deleteSpot = function(req, res) {
+  var id = req.params.id;
+  console.log('Deleting spot: ' + id);
+  db.collection('spots', function(err, collection) {
+    collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+      if (err) {
+        res.send({'error':'An error has occurred - ' + err});
+      } else {
+        console.log('' + result + ' document(s) deleted');
+        res.send(req.body);
+      }
+    });
+  });
+};
+
+var fillDb = function () {
+  var spots = [{
+    title: 'pothole',
+    latitude: 57.68313,
+    longitude: 11.9759733
+  }, {
+    title: 'blockage',
+    latitude: 57.6834508,
+    longitude: 11.9721963
+  }];
+
+  db.collection('spots', function (err, collection) {
+    collection.insert(spots, {safe: true}, function (err, result) {
+    });
+  });
+};
